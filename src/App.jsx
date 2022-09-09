7e567d1bef0381852b9691ee1b7fb08df3c75849
Saran-pariyar <spariyar05@gmail.com>
2022-09-09 17:10:33 +0545

    Made some files and added tailwind in project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import "@fontsource/poppins";
 import PopularMovie from "./components/PopularMovie";
+import Search from "./components/Search";
+import Result from "./components/Result";
 // import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Search />
+      <Result />
     </div>
   );
 }
---
+++ src/components/Result.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Result = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Result
