ad80b95bed2ba40cfd1b73974c27166338219493
Saran-pariyar <spariyar05@gmail.com>
2022-09-26 07:29:56 +0545

    Increased  hero image height in mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import hero_img from '../assets/hero-img.jpg'
 const Hero = () => {
   return (
-    <div>
+    <div className='my-1'>
         <h1 className='text-2xl font-bold text-center sm:text-4xl'>Welcome to MoviesGO!</h1>
-        <img src={hero_img} alt="hero" className='w-full mt-2' />
+        <img src={hero_img} alt="hero" className='h-[25vh] mt-2 sm:w-full sm:h-auto' />
     </div>
   )
 }
