e2876b40bd19db5b09bbb6a71adcb5f41cc092d0
Saran-pariyar <spariyar05@gmail.com>
2022-09-27 06:42:53 +0545

    Added image in explore section

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -30,26 +30,26 @@ const Explore = () => {
     <section className="explore my-3">
       <h1 className="m-4 text-3xl"><Slide left> Categories</Slide> </h1>
       <Flip top>
-      <div className={` my-3 overflow-x-scroll h-32  flex  items-center  border p-2 sm:block sm:h-auto sm:overflow-x-visible sm:border-none ${ theme === 'dark' ? "bg-slate-900" :"bg-slate-400 sm:bg-white"}`}>
+      <div className={` mt-3 overflow-x-scroll h-32  flex  items-center  border p-2 sm:block sm:h-auto sm:overflow-x-visible sm:border-none ${ theme === 'dark' ? "bg-slate-900" :"bg-slate-400 sm:bg-white"}`}>
         {genres.map((element) => {
           return (
-            <>
+            <div key={element.id}>
               <button
                 onClick={() => {
                   onClickHandler(element.id, element.name);
                 }}
-                key={element.id}
+                
                 className={` font-bold uppercase px-7 py-3 rounded-full outline-none focus:outline-none mr-1  ease-linear transition-all duration-150 sm:mb-4 ${ theme === 'dark' ? "text-white bg-slate-500 border-white hover:bg-white hover:text-slate-500" : "text-slate-500 border bg-white border-slate-500 hover:bg-slate-500 hover:text-white active:bg-slate-600"}`}
                 type="button"
               >
                 {element.name}
               </button>
-            </>
+            </div>
           );
         })}
       </div>
       </Flip>
-      {showResult ? <Result /> : ""}
+      {showResult ? <Result /> : <><img src="https://hdwallpaperim.com/wp-content/uploads/2017/08/22/99262-anime-colorful-original_characters.jpg" alt="explore" className="sm:h-auto" /></>}
     </section>
   );
 };
