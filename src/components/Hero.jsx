5c02a021d8c092fc1b8306bc05283a2acf8777a3
Saran-pariyar <spariyar05@gmail.com>
2022-09-30 08:26:45 +0545

    Made hero component all items placed in center

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { Fade } from 'react-reveal';
 
 const Hero = () => {
   return (
-    <div className='my-1'>
+    <div className='my-1 flex flex-col justify-center'>
         <Tada> <h1 className='text-2xl font-bold text-center sm:text-4xl'>Welcome to MoviesGO!</h1></Tada>
-        <Fade bottom> <img src={hero_img} alt="hero" className='h-[25vh] mt-2 sm:w-full sm:h-auto' /></Fade>
+        <Fade bottom > <img src={hero_img} alt="hero" className='h-[25vh] mt-2 sm:w-full sm:h-auto' /></Fade>
     </div>
   )
 }
